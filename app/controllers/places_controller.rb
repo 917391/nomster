99e2e96b850b607f69dfa1c18bdb07b52e220d43
917391 <quintonchinouya@gmail.com>
2019-08-29 21:43:23 +0000

    Creating the place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,14 @@
 class PlacesController < ApplicationController
 
     def index
-    @places = Place.all
+        @places = Place.all.paginate(page: params[:page], per_page: 10)
+        respond_to do |format|
+            format.html
+            format.json { render json: @places }
     end
-end
+    end
+    
+    def new
+        @place = Place.new
+    end
+ end
