1dd72fcea8a14b35c0371831542173789f6f3899
917391 <quintonchinouya@gmail.com>
2019-09-05 02:25:23 +0000

    adding a google map

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,8 @@
 class Place < ApplicationRecord
     belongs_to :user
+    geocoded_by :address
+    after_validation :geocode
+    validates :name, length: {minimum: 2}
+    validates :description, presence: true
+    validates :address, presence: true
 end
